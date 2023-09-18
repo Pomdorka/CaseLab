f554a1dd1b5916daf220a42cdf3acd7a1e62d01e
Pomdorka <andreychetvertakov@mail.ru>
2023-09-18 20:38:35 +0300

    qwe

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-/*var myNodelist = document.getElementsByTagName("LI");
+///window.onload = loadData;
+var myNodelist = document.getElementsByTagName("LI");
 var i;
+var globalLength = 0;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -7,49 +9,84 @@ for (i = 0; i < myNodelist.length; i++) {
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
-*/
 
 
 //Добавляем элемент к списку
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("addArea").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("Введите название задачи:)");
+    } else {
+      document.getElementById("myUL").appendChild(li);
 
-    const ul = document.querySelector('#table');
-    const input = document.getElementById('addArea');
-    let itemsArray = localStorage.getItem('items') ?
-JSON.parse(localStorage.getItem('items')) : [];
-itemsArray.forEach(addTask);
-function addTask(text){
-  const li = document.createElement('li')
-  li.textContent = text;
-  ul.appendChild(li);
-}
-function add(){
-  itemsArray.push(input.value);
-  localStorage.setItem('items', JSON.stringify(itemsArray));
-  addTask(input.value);
-  input.value = '';
-}
-function del(){
-  localStorage.clear();
-  ul.innerHTML = '';
-  itemsArray = [];
-}
-
+      
+    }
+    document.getElementById("addArea").value = "";
   
+    const ul = document.getElementById("myUL").getElementsByTagName("li");
+    let value = {
+    };
 
+    let j = 0;
+    for (j = 0; j <= ul.length - 1; j++) {
+      if (ul[j].innerHTML != ""){
+        if (localStorage.getItem("test") != null)
+          oldItem = localStorage.getItem("test");
+        else
+          oldItem = "";
+        ///alert(oldItem);
+        localStorage.removeItem("test");
+        
+        value[j] = {"setting" : ul[j].innerHTML};
+        
+        ///alert(value..setting);
+
+      }
+    }
+    globalLength = Object.keys(value).length;
+    //alert("hi");
+    
+    //let tevirp = value[0];
+    //console.log(globalLength);
+
+    localStorage.setItem('test', JSON.stringify(value));
+}
+
+function loadData() {
   
-  let LiArray = document.getElementsByClassName("myLi").value;
-  let arrayToStr = JSON.stringify(LiArray);
-  
-  localStorage.setItem('test', arrayToStr);
+  let value = JSON.parse(localStorage.getItem("test"));
+  var size = Object.keys(value).length;
+  //alert(value.first.setting);
+  ///alert(size);
+  for (let i = 0; i <= size - 1; i++) {
+      //console.log(value[i].setting);
+      const li = document.createElement("li");
+      document.getElementById("myUL").appendChild(li).innerHTML = value[i].setting;
+  }
+}
 
+function deleteLast(){
+  var ul = document.getElementById('myUL');
+  ul.removeChild(ul.lastElementChild);
+}
 
+function deleteFirst(){
+  var ul = document.getElementById('myUL');
+   ul.removeChild(ul.firstElementChild);
+}
 
-//Добавляем кнопку закрытия 
-/*var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  span.className = "fa-solid fa-xmark close";
-  myNodelist[i].appendChild(span);
-}*/
+function markOdd(){ //нечетные
+  var list = document.querySelectorAll('#myUL li');
+  for (var i = 0; i < list.length; i++) {
+    list[i].style.color = 'blue';
+  }
+}
+
+function markEven(){ //четные
+
+}
+
 
